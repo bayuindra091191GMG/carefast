ff05ee974ab4cb7a1f3e645d7632ef6fa7fe17f2
ferdyantorand <randf77@gmail.com>
2019-07-19 19:29:24 +0700

    Update Cart

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -69,6 +69,45 @@ class CartController extends Controller
     }
 
     /**
+     * Function to Update item in Cart.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateCart(Request $request)
+    {
+        try{
+            $rules = array(
+                'product_id'    => 'required',
+                'qty'           => 'required'
+            );
+
+            $data = $request->json()->all();
+            $validator = Validator::make($data, $rules);
+            if ($validator->fails()) {
+                return response()->json($validator->messages(), 400);
+            }
+
+            $user = auth('api')->user();
+            $product = Product::find($request->input('product_id'));
+            $totalPrice = $product->price * $request->input('qty');
+
+            $tmpCart = Cart::where('user_id', $user->id)->where('product_id', $product->id)->get();
+            if($tmpCart == null){
+                return response()->json("Cart tidak ditemukan!", 404);
+            }
+            $tmpCart->total_price = $totalPrice;
+            $tmpCart->qty = $request->input('qty');
+            $tmpCart->save();
+
+            return Response::json("Berhasil mengubah jumlah barang di keranjang belanja!", 200);
+        }
+        catch (\Exception $ex){
+            return Response::json("Maaf terjadi kesalahan!", 500);
+        }
+    }
+
+    /**
      * Function to Add item or Update Cart.
      *
      * @param Request $request
@@ -93,7 +132,7 @@ class CartController extends Controller
             if($tmpCart == null){
                 return response()->json("Cart tidak ditemukan!", 404);
             }
-            $tmpCart->save();
+            $tmpCart->delete();
 
             return Response::json("Berhasil menghapus dari keranjang belanja!", 200);
         }
