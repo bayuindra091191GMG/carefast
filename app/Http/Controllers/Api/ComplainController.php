25fd3c075804a9b6fa1e0a8d33ad1beac65c858a
yansen626 <yansen626@gmail.com>
2019-10-01 16:58:12 +0700

    01 oktober - edit complain

--- app/Http/Controllers/Api/ComplainController.php
+++ app/Http/Controllers/Api/ComplainController.php
@@ -31,7 +31,6 @@ class ComplainController extends Controller
         try{
             $rules = array(
                 'project_id'      => 'required',
-                'customer_id'   => 'required',
                 'subject'   => 'required',
                 'message'   => 'required'
             );
@@ -44,12 +43,12 @@ class ComplainController extends Controller
 
             $user = auth('customer')->customer();
 
-            $customer = Customer::find($request->input('customer_id'));
-            
+            $customer = Customer::find($user->id);
+
             //create customer complaint
             $newComplaint = CustomerComplaint::create([
                 'project_id'   => $request->input('project_id'),
-                'customer_id'   => $request->input('customer_id'),
+                'customer_id'   => $user->id,
                 'customer_name'   => $customer->name,
                 'subject'     => $request->input('subject'),
                 'date'          => Carbon::now('Asia/Jakarta')->toDateTimeString(),
@@ -61,11 +60,11 @@ class ComplainController extends Controller
 
             //get employee ID
             $employeeID = 1;
-            $employeeList = Employee::where('project_id', $request->input('project_id'))
+            $employeeDB = Employee::where('project_id', $request->input('project_id'))
                         ->where('employee_roles_id', '>', 1)
                         ->orderBy('employee_roles_id', 'asc')
                         ->first();
-            $employeeID = $employeeList->employee_id;
+            $employeeID = $employeeDB->employee_id;
 
             //create complaint detail
             $newComplaint = CustomerComplaintDetail::create([
@@ -92,68 +91,10 @@ class ComplainController extends Controller
         try{
             $user = auth('customer')->customer();
             $customer = Customer::find($user->id);
-            
-            $customerComplaints =  CustomerComplaint::where('customer_id', $customer->id)
-            
-            -> get();
-            
+
+            $customerComplaints =  CustomerComplaint::where('customer_id', $customer->id)->get();
+
             return Response::json($customerComplaints, 200);
-        }
-        catch (\Exception $ex){
-            return Response::json("Maaf terjadi kesalahan!", 500);
-        }
-    }
-
-    public function employeeList(Request $request){
-        try{
-            $rules = array(
-                'qr_code'   => 'required'
-            );
-
-            $data = $request->json()->all();
-            $validator = Validator::make($data, $rules);
-            if ($validator->fails()) {
-                return response()->json($validator->messages(), 400);
-            }
-
-            //Submit Data
-            $place = Place::where('qr_code', $request->input('qr_code'))->first();
-            $date = Carbon::now('Asia/Jakarta');
-            $time = $date->format('H:i:s');
-            $schedule = Schedule::where('place_id', $place->id)->where('finish' <= $time)->with('employee')->get();
-
-            return Response::json([
-                'message'   => 'Sukses mengambil data Schedule!',
-                'model'     => $schedule
-            ]);
-        }
-        catch (\Exception $ex){
-            return Response::json("Maaf terjadi kesalahan!", 500);
-        }
-    }
-
-    public function supervisorSubmit(Request $request){
-        try {
-            $rules = array(
-                'attendance_id' => 'required'
-            );
-
-            $data = $request->json()->all();
-            $validator = Validator::make($data, $rules);
-            if ($validator->fails()) {
-                return response()->json($validator->messages(), 400);
-            }
-
-            //Submit Data
-            $place = Place::where('qr_code', $request->input('qr_code'))->first();
-
-            if($place->qr_code != Crypt::decryptString($request->input('qr_code'))){
-                return Response::json("Tempat yang discan tidak tepat!", 400);
-            }
-            $date = Carbon::now('Asia/Jakarta');
-            $time = $date->format('H:i:s');
-            $schedule = Schedule::where('place_id', $place->id)->where('start' >= $time)->where('finish' <= $time)->first();
-//            $attendance = Attendance::where('schedule_id', $schedule->id)->where('employee_id', )
         }
         catch (\Exception $ex){
             return Response::json("Maaf terjadi kesalahan!", 500);
