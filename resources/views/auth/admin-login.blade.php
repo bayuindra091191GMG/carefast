4f8630fc6ff4b0af6deec86adaa10fbca21b7040
yansen626 <yansen626@gmail.com>
2021-09-17 09:53:48 +0700

    17 september - edit description meta di login

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Digital Waste Solution backend">
+    <meta name="description" content="CAREFAST BACKEND">
     <meta name="author" content="Generasi Muda Gigih">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend/assets/images/favicon.png') }}">
